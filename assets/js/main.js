37145a80a98e4f73beb31eb75d9aab95115e8d42
efeegrekci <efe.egrekci@gmail.com>
2022-03-13 06:14:57 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+    // Resize Page Refresh
+    $(window).bind('resize', function (e) {
+        if (window.RT) clearTimeout(window.RT);
+        window.RT = setTimeout(function () {
+            this.location.reload(false);
+        }, 100);
+    });
+    // Resize Page Refresh
+
+    // Tabs
+    $('.hamburgerMenuButton').click(function () {
+        $(".hamburgerMenuList").addClass("active")
+    });
+    $('.closeButton').click(function () {
+        $(".hamburgerMenuList").removeClass("active")
+    });
+    // Tabs
+
+});
